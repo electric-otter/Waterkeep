c7d7544af0ea58064b48516588791e812cba1ea4
Shreya <shreyadavhale@gmail.com>
2024-10-10 22:09:58 +0530

    Updated App component and added App.css styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import "./App.css"; 
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
